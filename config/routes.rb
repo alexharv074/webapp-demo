bfd9d58beadaefe9706691038451f26f7a7d63ac
Alex Harvey <alexharv074@gmail.com>
2016-05-26 20:27:43 +1000

    State prior to DB table creation
    Progress so far:
    
    $ bin/rails generate controller articles
    
    Sets the root in routes
    Adds a resource for :articles to routes
    Adds controller, template, first form, create method
    
    $ bin/rails generate model Article title:string text:text

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :articles
 
   # Example resource route with options:
   #   resources :products do
